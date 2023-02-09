90373307a0de647adda38a378f9de93702863fe7
vckindarkhediya <vipulkindarkhediya1204@gmail.com>
2023-02-09 11:31:26 +0530

    create database

---
+++ Day-1-Learning/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+      
+     <!--Syntax  -->
+     <!-- create database tablename; -->
+     create database learningmysql;
+</body>
+</html>
+
+
